fd20e58c4d916a2eb31b90e752bfc009c667b4e5
Yusuf <mohdyusufhesam@gmail.com>
2024-08-05 00:23:50 +0530

    setup next with tailwind, shadCN & and custom classes

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+ return(
+  <div>
+    <h1 className="text-3xl underline">home</h1>
+    <Button> click me</Button>
+  </div>
+ );
+}
